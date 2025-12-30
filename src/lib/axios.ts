f5f57bf8075a934927d3a51c621e186824662a7f
10000jaeN <akswo256@naver.com>
2025-12-30 13:46:14 +0900

    feat: add axios instance and interceptors

---
+++ src/lib/axios.ts
@@ -0,0 +1,41 @@
+import axios, {
+  AxiosError,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000",
+  timeout: 1000,
+  headers: { "Content-Type": "application/json" },
+});
+
+axiosInstance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem("accessToken");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+    return config;
+  },
+  (error: AxiosError) => {},
+);
+
+axiosInstance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    const newToken = response.headers["x-new-access-token"];
+    if (newToken) {
+      localStorage.setItem("accessToken", newToken);
+    }
+    return response;
+  },
+  (error: AxiosError) => {
+    if (error.status === 401) {
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
