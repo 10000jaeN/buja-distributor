441a9e32f2dc2e0329ffda2efe8fc6736f6f0202
10000jaeN <akswo256@naver.com>
2026-01-01 17:52:58 +0900

    feat: add Nav and Footer in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,19 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import './globals.css';
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import Nav from "@/components/common/Nav";
+import Footer from "@/components/common/Footer";
 
 const pretendard = localFont({
-  src: './fonts/PretendardVariable.woff2',
-  display: 'swap',
-  weight: '45 920', // 가변 폰트의 범위를 지정합니다.
-  variable: '--font-pretendard',
+  src: "../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920", // 가변 폰트의 범위를 지정합니다.
+  variable: "--font-pretendard",
 });
 
 export const metadata: Metadata = {
-  title: '부자유통',
-  description: '유통을 내 손 안에',
+  title: "부자유통",
+  description: "유통을 내 손 안에",
 };
 
 export default function RootLayout({
@@ -21,7 +23,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" className={pretendard.variable}>
-      <body className="font-sans antialiased">{children}</body>
+      <body className="flex min-h-screen flex-col font-sans antialiased">
+        <Nav />
+
+        <main className="flex-1">{children}</main>
+
+        <Footer />
+      </body>
     </html>
   );
 }
