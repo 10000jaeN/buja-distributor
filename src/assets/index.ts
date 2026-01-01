a89280e9b4b06766020e1de8ea22281c47d8a701
10000jaeN <akswo256@naver.com>
2026-01-01 17:50:58 +0900

    feat: add Logo svg (naver, kakao, google)

--- src/assets/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Logo } from "./Icon-logo.svg";
-export { default as ArrowIcon } from "./Icon-arrow.svg";
-export { default as CartIcon } from "./Icon-cart.svg";
-export { default as MenuIcon } from "./Icon-hamberger.svg";
-export { default as SearchIcon } from "./Icon-search.svg";
-export { default as CancelIcon } from "./Icon-x.svg";
