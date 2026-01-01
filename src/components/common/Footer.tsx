c666eedf30c458660d2faf945e717de81028498a
10000jaeN <akswo256@naver.com>
2026-01-01 17:46:49 +0900

    feat: add Footer prototype component

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-black px-2 py-5 text-sm text-gray-400">
+      대표: 김동빈 ) 010-4539-9003
+      <br />
+      강원도 춘천시
+    </footer>
+  );
+};
+
+export default Footer;
