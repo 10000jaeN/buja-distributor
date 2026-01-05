28726d899fd21d4bd89d0ea5c303c7b350e3a2d2
10000jaeN <akswo256@naver.com>
2026-01-05 11:09:56 +0900

    refactor: Nav z-index

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="sticky top-0 flex h-17.25 items-center justify-between bg-white p-5">
+    <nav className="sticky top-0 z-50 flex h-17.25 items-center justify-between bg-white p-5">
       <MenuIcon className="h-6 w-6" />
       <Link href="/" className="absolute left-1/2 -translate-x-1/2">
         <Logo className="w-[102px]" shapeRendering="crispEdges" />
