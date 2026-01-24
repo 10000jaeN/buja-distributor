71927244238479905bf57eba9c703294898026e3
10000jaeN <akswo256@naver.com>
2026-01-24 16:22:31 +0900

    style: footer margin

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-black px-2 py-5 text-sm text-gray-400">
+    <footer className="bg-black px-3 py-5 text-sm text-gray-400">
       대표: 김동빈 ) 010-4539-9003
       <br />
       강원도 춘천시
