a65aaaee4cb36d0e4040088ba97aad6d5f7c8d25
10000jaeN <akswo256@naver.com>
2026-01-24 17:40:05 +0900

    refactor: add LoginSuccessHandler for useSearchParams(CSR Bailout)

--- src/app/login/success/page.tsx
+++ src/app/login/success/page.tsx
@@ -1,57 +1,16 @@
-"use client";
-
-import axiosInstance from "@/lib/axios";
-import useAuthStore from "@/store/useAuthStore";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useRef } from "react";
-
-interface ExchangeResponse {
-  accessToken: string;
-  userId: string;
-  nickName: string;
-}
+import { Suspense } from "react";
+import LoginSuccessHandler from "./components/LoginSuccessHandler";
 
 const LoginSuccess = () => {
-  const searchParams = useSearchParams();
-  const ticket = searchParams.get("ticket");
-  const router = useRouter();
-  const { login, isLoggedIn } = useAuthStore();
-  const hasCalled = useRef(false);
-
-  useEffect(() => {
-    if (!ticket || hasCalled.current || isLoggedIn) return;
-
-    const exchange = async () => {
-      try {
-        const res = await axiosInstance.post<ExchangeResponse>(
-          "/auth/exchange",
-          { ticket },
-        );
-        const { accessToken, userId, nickName } = res.data;
-
-        if (res.data.accessToken) {
-          localStorage.setItem("accessToken", accessToken);
-          axiosInstance.defaults.headers.common["Authorization"] =
-            `Bearer ${accessToken}`;
-        }
-
-        login({ userId, nickName });
-
-        router.replace("/");
-      } catch (err) {
-        console.error("인증 실패: ", err);
-        alert("로그인에 실패했습니다. 다시 시도해주세요.");
-        router.replace("/login");
-      }
-    };
-
-    exchange();
-  }, [ticket, router, isLoggedIn]);
-
   return (
-    <div className="mt-20 flex justify-center text-[20px] font-bold">
-      로그인 하는 중...
-    </div>
+    <>
+      <h1 className="mt-20 flex justify-center text-[20px] font-bold">
+        로그인 하는 중...
+      </h1>
+      <Suspense fallback={<div>로딩중...</div>}>
+        <LoginSuccessHandler />
+      </Suspense>
+    </>
   );
 };
 
